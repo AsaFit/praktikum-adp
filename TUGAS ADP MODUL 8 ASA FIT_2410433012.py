96218d7804dc23aa2be821246155d8855bca3f5d
AsaFit <asafit082005@gmail.com>
2025-06-10 01:05:53 +0700

    Add files via upload

--- TUGAS ADP MODUL 8 ASA FIT_2410433012.py
+++ TUGAS ADP MODUL 8 ASA FIT_2410433012.py
@@ -1,4 +1,3 @@
-
 def baca_baris(file):
     hasil = ""
     while True:
@@ -26,25 +25,26 @@ def konversi_int(s):
     angka = 0
     i = 0
     while i < len(s):
-        if s[i] == "0":
+        c = s[i]
+        if c == "0":
             digit = 0
-        elif s[i] == "1":
+        elif c == "1":
             digit = 1
-        elif s[i] == "2":
+        elif c == "2":
             digit = 2
-        elif s[i] == "3":
+        elif c == "3":
             digit = 3
-        elif s[i] == "4":
+        elif c == "4":
             digit = 4
-        elif s[i] == "5":
+        elif c == "5":
             digit = 5
-        elif s[i] == "6":
+        elif c == "6":
             digit = 6
-        elif s[i] == "7":
+        elif c == "7":
             digit = 7
-        elif s[i] == "8":
+        elif c == "8":
             digit = 8
-        elif s[i] == "9":
+        elif c == "9":
             digit = 9
         else:
             return -1
@@ -52,15 +52,23 @@ def konversi_int(s):
         i = i + 1
     return angka
 
+def file_bisa_dibuka(nama_file):
+    hasil = False
+    f = open("tes_file.txt", "r")
+    semua = ""
+    while True:
+        c = f.read(1)
+        if c == "":
+            break
+        semua = semua + c
+    if semua != "":
+        hasil = True
+    f.close()
+    return hasil
+
 def load_data(nama_file):
     data = {}
-    f = None
-    try:
-        f = open(nama_file, "r")
-    except:
-        # Kalau file tidak ada, data tetap kosong
-        return data
-
+    f = open(nama_file, "r")
     while True:
         line = baca_baris(f)
         if line == "":
@@ -79,8 +87,7 @@ def load_data(nama_file):
                     "jumlah": jumlah,
                     "tipe": tipe
                 }
-    if f is not None:
-        f.close()
+    f.close()
     return data
 
 def simpan_data(data, nama_file):
@@ -152,9 +159,6 @@ def hapus_data(data):
             print("Penghapusan dibatalkan.")
     else:
         print("Nomor data tidak ditemukan.")
-
-
-# Mulai program di sini, langsung jalan tanpa fungsi main()
 
 NAMA_FILE = "data_keuangan.txt"
 data_keuangan = load_data(NAMA_FILE)
